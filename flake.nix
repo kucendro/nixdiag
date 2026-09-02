4a281485dd84e59d654f2c6a5e7482ed317de385
Ondřej Kučera <kucera@deepvision.cz>
2026-09-02 13:03:56 +0200

    formatting fix

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
           flake = fixtureFlake;
           title = "Example fleet";
           domains.ts = "ts.example";
-          closures = true;
+          # closures = true;
         };
 
         site =
